806ac0e9eaa7be1ec16272a7af9e0749cc10d195
Alexandre Delmer <adelmer@arsenal.arsenal.fr>
2023-11-02 14:07:29 +0100

    MàJ 02/11/2023

--- script/script.js
+++ script/script.js
@@ -1,5 +1,26 @@
-let userChoice = document.querySelectorAll('.game input')
+// fonction pour afficher la proposition
 
-function 
+let choice = "vide"
 
-console.log(userChoice)
+function afficherProposition() {
+    let inputChoice = document.querySelectorAll('input[name="option"]')
+    let propositionArea = document.querySelector('.propositionArea')
+    
+    for( let i = 0; i < inputChoice.length; i++){
+        inputChoice[i].addEventListener('click', function () {
+            if(inputChoice[i].checked){
+                let choice = inputChoice[i].id
+            }
+        })    
+    }
+}
+
+afficherProposition()
+
+console.log(choice)
+
+// fonction pour afficher le résultat
+
+let score = document.querySelector('.score span')
+
+// fonction global du jeu
