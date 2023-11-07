5a86b20f44f89084eb96860d9fe4b72a07d4368d
Alexandre Delmer <delmer.alexandre1@gmail.com>
2023-11-07 22:06:57 +0100

    MàJ 07/11/2023 22:06

--- script/script.js
+++ script/script.js
@@ -1,11 +1,15 @@
+// Liste des variables utiles dans dans plusieurs fonction
+let choice = ""
+let score = 0
+let listeProposition = listeMots
+
 // fonction pour afficher la proposition
 
 function afficherProposition() {
     let inputChoice = document.querySelectorAll('input[name="option"]')
     let propositionArea = document.querySelector('.propositionArea')
     let totalClass = document.querySelector('.total')
-    let choice = "mots"
-    
+
     for( let i = 0; i < inputChoice.length; i++){
         inputChoice[i].addEventListener('click', function () {
             if(inputChoice[i].checked){
@@ -14,16 +18,11 @@ function afficherProposition() {
             
             if(choice === "mots"){
                     propositionArea.innerHTML = listeMots[0]
+                    totalClass.innerHTML = listeMots.length
                 } else {
                     propositionArea.innerHTML = listePhrases[0]
+                    totalClass.innerHTML = listePhrases.length
                 }
-
-            if (choice === "mots"){
-                totalClass.innerHTML = listeMots.length
-            } else {
-                totalClass.innerHTML = listePhrases.length
-            }
-            
         })    
     }
 }
@@ -39,11 +38,26 @@ function afficherResultat(score) {
 // fonction global pour lancer le jeu
 
 function lancerJeu() {
-    let score = 0
+    let buttonZoneProposition = document.querySelector('.zoneProposition button')
 
     afficherResultat(score)
 
     afficherProposition()
 
+// ok mais trouver un moyen de mettre à jour listeProposition et de faire une boucle (voir pour utiliser choice) sinon il faut comprendre comment mettre à jour un variable dans la function afficherProposition
+        buttonZoneProposition.addEventListener('click', function () {
+            let inputZoneProposition = document.querySelector('input[name="proposition"]')
+            
+            if(inputZoneProposition.value === propositionArea.textContent){
+            score++
+            }
 
+            afficherResultat(score)
+
+            inputZoneProposition.value = ""
+
+            console.log(listeProposition)
+
+        })
 }
+
