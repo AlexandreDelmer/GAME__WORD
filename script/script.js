ba9f7d2881ad962efb4a1fcfd8bfbc0bdd3660dc
Alexandre Delmer <adelmer@arsenal.arsenal.fr>
2023-11-09 14:24:39 +0100

    MàJ 09/11/2023 14:24

--- script/script.js
+++ script/script.js
@@ -1,35 +1,37 @@
 // Liste des variables utiles dans dans plusieurs fonction
-let choice = ""
-let score = 0
-let listeProposition = listeMots
 
-// fonction pour afficher la proposition
+// Fonction pour obtenir la choix de l'utilisateur
+function userChoice () {
+    let inputMots = document.getElementById("mots")
+    let inputPhrases = document.getElementById("phrases")
 
-function afficherProposition() {
-    let inputChoice = document.querySelectorAll('input[name="option"]')
-    let propositionArea = document.querySelector('.propositionArea')
-    let totalClass = document.querySelector('.total')
-
-    for( let i = 0; i < inputChoice.length; i++){
-        inputChoice[i].addEventListener('click', function () {
-            if(inputChoice[i].checked){
-                choice = inputChoice[i].id
-            }
-            
-            if(choice === "mots"){
-                    propositionArea.innerHTML = listeMots[0]
-                    totalClass.innerHTML = listeMots.length
-                } else {
-                    propositionArea.innerHTML = listePhrases[0]
-                    totalClass.innerHTML = listePhrases.length
-                }
-        })    
+    if(inputMots.checked){
+        choice = inputMots.id
+    } else {
+        choice = inputPhrases.id
     }
+
+    return choice
 }
 
 
-// fonction pour mettre à jour le resultat
+// fonction pour afficher la proposition
+function propositionDisplay (choice) {
+    let propositionArea = document.querySelector(".propositionArea")
+    
+    if(choice === "mots"){
+        propositionArea.textContent = listeMots[0]
+    } else {
+        propositionArea.textContent = listePhrases[0]
+    }
+}
 
+// fonction pour mettre à jours l'affichage de la proposition
+function majPropositionDisplay (choice) {
+    let inputChoice = document.getElementById
+}
+
+// fonction pour mettre à jour le resultat
 function afficherResultat(score) {  
     const scoreDisplay = document.querySelector('.score')
     scoreDisplay.innerHTML = score
@@ -38,26 +40,14 @@ function afficherResultat(score) {
 // fonction global pour lancer le jeu
 
 function lancerJeu() {
-    let buttonZoneProposition = document.querySelector('.zoneProposition button')
+    let score = 0
+    let choice = userChoice()
 
     afficherResultat(score)
 
-    afficherProposition()
+    propositionDisplay(choice)
+    
+    console.log(choice)
 
-// ok mais trouver un moyen de mettre à jour listeProposition et de faire une boucle (voir pour utiliser choice) sinon il faut comprendre comment mettre à jour un variable dans la function afficherProposition
-        buttonZoneProposition.addEventListener('click', function () {
-            let inputZoneProposition = document.querySelector('input[name="proposition"]')
-            
-            if(inputZoneProposition.value === propositionArea.textContent){
-            score++
-            }
-
-            afficherResultat(score)
-
-            inputZoneProposition.value = ""
-
-            console.log(listeProposition)
-
-        })
 }
 
