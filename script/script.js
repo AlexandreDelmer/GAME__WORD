79bfc45657c95cffe21f27590989fe0df72dd34b
Alexandre Delmer <adelmer@arsenal.arsenal.fr>
2023-11-13 17:53:39 +0100

    MàJ 13/11/2023 17:53

--- script/script.js
+++ script/script.js
@@ -1,37 +1,36 @@
 // Liste des variables utiles dans dans plusieurs fonction
 
 // Fonction pour obtenir la choix de l'utilisateur
-function userChoice () {
-    let inputMots = document.getElementById("mots")
-    let inputPhrases = document.getElementById("phrases")
 
-    if(inputMots.checked){
-        choice = inputMots.id
-    } else {
-        choice = inputPhrases.id
-    }
+// function pour changer les valeurs de la zone de proposition en fonction de la valeur checked des input radio
 
-    return choice
-}
+function propositionDisplay(){
+    let inputChoice = document.querySelectorAll('input[name="option"')
+    let propositionArea = document.querySelector('.propositionArea')
+    let totalScore = document.querySelector('.total')
 
-// fonction pour afficher la proposition
-function propositionDisplay (choice) {
-    let propositionArea = document.querySelector(".propositionArea")
-    let total = document.querySelector(".total")
-    
-    if(choice === "mots"){
-        propositionArea.textContent = listeMots[0]
-        total.innerHTML = listeMots.length
-    } else if(choice === "phrases"){
-        propositionArea.textContent = listePhrases[0]
-        total.innerHTML = listePhrases.length
+    for(let i = 0; i < inputChoice.length; i++){
+        inputChoice[i].addEventListener('click', () => {
+            let userChoice = ""
+
+            if(inputChoice[i].checked){
+            userChoice = inputChoice[i].id
+        }
+
+            if(userChoice === "mots"){
+                propositionArea.textContent = listeMots[0]
+                totalScore.textContent = listeMots.length
+            } else if(userChoice === "phrases"){
+                propositionArea.textContent = listePhrases[0]
+                totalScore.textContent = listePhrases.length
+            }
+            
+            return userChoice
+        })
     }
 }
 
 // fonction pour mettre à jours l'affichage de la proposition
-function majPropositionDisplay (choice) {
-    let inputChoice = document.getElementById
-}
 
 // fonction pour mettre à jour le resultat
 function resultDisplay(score) {  
@@ -43,13 +42,13 @@ function resultDisplay(score) {
 
 function lancerJeu() {
     let score = 0
-    let choice = userChoice()
+    let userChoice
 
     resultDisplay(score)
 
-    propositionDisplay(choice)
+    propositionDisplay()
     
-    console.log(choice)
+    console.log(userChoice)
 
 }
 
