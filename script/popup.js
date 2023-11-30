0729a1e99b118466792a6b818529f03a006f69ed
Alexandre Delmer <adelmer@arsenal.arsenal.fr>
2023-11-30 16:58:05 +0100

    MàJ 30/11/2023 16:58

--- script/popup.js
+++ script/popup.js
@@ -3,8 +3,7 @@ let overlay = document.querySelector('.overlay')
 
 
 // Problème : rien ne se passe au click 
-popup.addEventListener('click', function(event){
-    console.log("1")
+window.addEventListener('click', function(event){
     if(!popup.contains(event.target)){
         popup.style.display = "none"
         overlay.style.display = "none"
