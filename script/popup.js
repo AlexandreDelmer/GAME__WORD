56e8caf7ffff194f5aa7b3e677fd96efecf179a2
unknown <delmer.alexandre0@gmail.com>
2023-12-03 08:49:39 +0100

    MàJ 03/12/2023 8:49

--- script/popup.js
+++ script/popup.js
@@ -1,13 +1,29 @@
 let popup = document.querySelector('.popup')
 let overlay = document.querySelector('.overlay')
+let popupActive = false
+let btnPartager = document.getElementById("btnPartager")
 
 
-// Problème : rien ne se passe au click 
-window.addEventListener('click', function(event){
-    if(!popup.contains(event.target)){
-        popup.style.display = "none"
-        overlay.style.display = "none"
-    }
+for(let i = 0; i<2; i++){
+// Active la popup au click sur le bouton partager
+btnPartager.addEventListener('click', () => {
+        popup.style.display = "flex"
+        overlay.style.display = "flex"
+        popupActive = true
+        console.log(popupActive)
     }
 )
 
+// Désactivation de la popup au click en dehors de celle-ci
+if(popupActive){
+window.addEventListener('click', function(event){
+        if(!popup.contains(event.target)){
+            popup.style.display = "none"
+            overlay.style.display = "none"
+            popupActive = false
+            console.log(popupActive)
+        }
+    }
+)   
+}
+}
