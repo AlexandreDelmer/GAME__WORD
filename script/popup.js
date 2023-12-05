645622c0f5975e40158e27183a743cead3aa219f
Alexandre Delmer <delmer.alexandre1@gmail.com>
2023-12-05 21:23:30 +0100

    05/12/2023 21:23

--- script/popup.js
+++ script/popup.js
@@ -2,9 +2,13 @@ let popup = document.querySelector('.popup')
 let overlay = document.querySelector('.overlay')
 let popupActive = false
 let btnPartager = document.getElementById("btnPartager")
+let btnEnvoyer = document.getElementById("btnEnvoyer")
 let nom = document.getElementById("nom")
 let prenom = document.getElementById("prenom")
 let email = document.getElementById("email")
+let inputPopup = document.querySelectorAll('.popup input')
+const regex = /^[A-Z][a-z\-]{0,19}$/
+const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
 
 
 // Active la popup au click sur le bouton partager
@@ -28,22 +32,45 @@ window.addEventListener('click', function(event){
     }
 }) 
 
-// Function pour ouvrir la messagerie avec un email prérempli au click sur le bouton Envoyer
-let btnEnvoyer = document.getElementById("btnEnvoyer")
+// Function pour valider un input 
+function validateInput(input, regex){
+    let inputValue = input.value
 
-const regex = /^[A-Z][a-z\-]{0,19}$/
-let prenomValue = prenom.value
-let emailValue = email.value
-
-nom.addEventListener('focusout', 'keydown', () =>{
-    let nomValue = nom.value
-    if(!regex.test(nomValue)){
-        nom.classList.add('popup__clignotant__animation')
+    if(!regex.test(inputValue)){
+        console.log("Validation échouée")
+        input.classList.add('popup__clignotant__animation')
 
         setTimeout(function (){
-            nom.classList.remove('popup__clignotant__animation');
-        },  4000)
-    } 
+            input.classList.remove('popup__clignotant__animation');
+        },  4000);
+
+        return false
+    }
+
+    return true
+}
+
+nom.addEventListener('blur', () =>{
+    validateInput(nom, regex)
 })
 
+prenom.addEventListener('blur', () =>{
+    validateInput(prenom, regex)
+})
+
+email.addEventListener('blur', () =>{
+    validateInput(email, regexEmail)
+})
+
+btnEnvoyer.addEventListener('click', () => {
+    let isNomValide = validateInput(nom, regex)
+    let isPrenomValide = validateInput(prenom, regex)
+    let isEmailvalide = validateInput(email, regexEmail)
+
+    if(isNomValide && isPrenomValide && isEmailvalide){
+        console.log("OK")
+    } else {
+        console.log("Un des inputs contient une erreur")
+    }
+})
 
