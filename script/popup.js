b1f2a6cd0fa5bf5cb03b4047bbfe241c470794c8
Alexandre Delmer <adelmer@arsenal.arsenal.fr>
2023-12-05 14:00:09 +0100

    MàJ 05/12/2023 13:59

--- script/popup.js
+++ script/popup.js
@@ -15,6 +15,9 @@ btnPartager.addEventListener('click', () => {
 
 // Désactivation de la popup au click en dehors de celle-ci
 window.addEventListener('click', function(event){
+    let nom = document.getElementById("nom")
+    nom.focus()
+
     if(popupActive){
         if(!popup.contains(event.target) && event.target !== btnPartager){
             popup.style.display = "none"
@@ -28,25 +31,26 @@ window.addEventListener('click', function(event){
 let btnEnvoyer = document.getElementById("btnEnvoyer")
 
 function sendEmail() {
+    const regex = /^[A-Z][a-z\-]{0,19}$/
+
     btnEnvoyer.addEventListener('click', () => {
-        let nom = document.getElementById("nom").value
-        let prenom = document.getElementById("prenom").value
-        let email = document.getElementById("email").value
-        
-        console.log(nom, prenom, email)
+        let nomValue = document.getElementById("nom").value
+        let prenomValue = document.getElementById("prenom").value
+        let emailValue = document.getElementById("email").value
+
+        let nom = document.getElementById("nom")
+        let prenom = document.getElementById("prenom")
+        let email = document.getElementById("email")
+
+        if(!regex.test(nomValue)){
+            nom.classList.add('popup__clignotant__animation')
+
+            setTimeout(function (){
+                nom.classList.remove('popup__clignotant__animation');
+            },  4000)
+        }
     })
 }
 
 sendEmail ()
-
-// Function pour valider les input
-function validateInput() {
-        const regex = /^[A-Z][a-z\-]{0,19}$/
-        let nom = document.getElementById("nom").value
-
-        if(!regex.test(nom)){
-                
-        }
-}
-
 
